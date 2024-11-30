74860ef8c130af20bc7890ada03030af26ef4ba8
Youngjoo <tyty4420@naver.com>
2024-11-30 15:45:50 +0900

    Initial Commit

---
+++ src/main/java/com/skmnservice/SkmnserviceApplication.java
@@ -0,0 +1,13 @@
+package com.skmnservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkmnserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkmnserviceApplication.class, args);
+	}
+
+}
