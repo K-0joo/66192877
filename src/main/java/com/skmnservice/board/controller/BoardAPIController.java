27035577012eca65135ae2516264f5fe382df2c4
Youngjoo <tyty4420@naver.com>
2024-12-02 09:55:13 +0900

    [ feat ] Board Controller 코드 작성

---
+++ src/main/java/com/skmnservice/board/controller/BoardAPIController.java
@@ -0,0 +1,28 @@
+package com.skmnservice.board.controller;
+
+import com.skmnservice.board.dto.BoardRequest;
+import com.skmnservice.board.dto.BoardResponse;
+import com.skmnservice.global.response.ResponseCode;
+import com.skmnservice.global.response.ResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/api/board")
+public class BoardAPIController {
+    private final BoardService boardService;
+
+    @PostMapping("/write")
+    @ResponseBody
+    public ResponseEntity<ResponseDto> write(@RequestBody BoardRequest requestDto){
+        BoardResponse responseDto = boardService.write(requestDto);
+        return ResponseEntity.ok(ResponseDto.of(ResponseCode.BOARD_SUCCESS, responseDto));
+
+    }
+}
